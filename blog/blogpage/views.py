42c01e5703feac6f2fef073a395463973f28f4c9
mthwbrwn <mthwbrwn@gmail.com>
2019-05-13 18:28:17 -0700

    templates added - home and about

--- blog/blogpage/views.py
+++ blog/blogpage/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Matthew',
+        'title': 'first post',
+        'content': 'This is the very first post.',
+        'date': 'dummy date'
+    },
+
+    {
+        'author': 'Matthew II',
+        'title': 'second post',
+        'content': 'This is yet another post.',
+        'date': 'dummy date II'
+    }
+
+
+]
+
 
 def home(request):
-    return render(request, 'blogpage/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blogpage/home.html', context)
 
 
 def about(request):
-    return render(request, 'blogpage/about.html')
+    return render(request, 'blogpage/about.html', {'title': 'about'})
