022c632c532331d45084ccb303e0b3f31510e8cb
mthwbrwn <mthwbrwn@gmail.com>
2019-05-14 15:42:39 -0700

    images added to blog eentries

--- blog/users/forms.py
+++ blog/users/forms.py
@@ -1,12 +1,27 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+
 
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(help_text='A valid email address, please.')
     
-
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(help_text='A valid email address, please.')
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+   
+    class Meta:
+        model = Profile
+        fields = ['image']
