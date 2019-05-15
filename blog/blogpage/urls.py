8045f8c40b1c3c1f5691c926f49d54165ee90d4d
mthwbrwn <mthwbrwn@gmail.com>
2019-05-14 20:51:34 -0700

    nav buttons added

--- blog/blogpage/urls.py
+++ blog/blogpage/urls.py
@@ -5,6 +5,7 @@ from .views import (
     PostDetailView,
     PostCreateView,
     PostUpdateView,
+    PostDeleteView,
 )
 
 urlpatterns = [
@@ -12,6 +13,7 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blogpage-about')
 ]
 
--- blog/blogpage/views.py
+++ blog/blogpage/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView,
     DetailView,
     CreateView,
     UpdateView,
+    DeleteView,
 )
 
 
@@ -36,13 +37,29 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
     return render(request, 'blogpage/about.html', {'title': 'about'})
